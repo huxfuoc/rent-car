7fcc5ca04b1a1615bc8fbd56cc2c14acd64d1950
huxfuoc <hphc0811@gmail.com>
2024-03-15 15:36:27 +0700

    setup Relative Path

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import Layout from './layout/Layout.jsx'
 import './index.scss'
-import App from './App'
-// import Login from './pages/Login.jsx'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Login /> */}
-    {/* <Layout /> */}
     <App />
   </React.StrictMode>,
 )
